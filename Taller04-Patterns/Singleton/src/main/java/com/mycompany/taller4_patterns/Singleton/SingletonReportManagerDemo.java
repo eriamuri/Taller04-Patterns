75abc51a1c829f4085e4526336b744e0bc08ec30
Jose Andres Adrian Fierro <jadrian@espol.edu.ec>
2024-11-07 00:33:58 -0500

    Singleton

---
+++ Taller04-Patterns/Singleton/src/main/java/com/mycompany/taller4_patterns/Singleton/SingletonReportManagerDemo.java
@@ -0,0 +1,7 @@
+package com.mycompany.taller4_patterns.Singleton;
+
+public class SingletonReportManagerDemo {
+    public static void main(String[] args) {
+        System.out.println("aqui se obtiene el singleton");
+    }
+}
