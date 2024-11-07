a53b1d7f803387b11a319ceeafe5144ae54556e0
Claudia Sofía Asanza Romero <ricky_240131@hotmail.com>
2024-11-06 22:22:37 -0500

    Patrón Adapter
    Implemente las clases y la interfaz para este patrón

---
+++ Taller4_Patterns/src/main/java/PatrónAdapter/Telegram.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PatrónAdapter;
+
+/**
+ *
+ * @author ricky
+ */
+public class Telegram {
+    public void notificar(String reporte){
+        System.out.println("Enviar reporte por Telegram:"+reporte);
+    }
+}
---
+++ Taller4_Patterns/src/main/java/PatrónAdapter/WhatsApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PatrónAdapter;
+
+/**
+ *
+ * @author ricky
+ */
+public class WhatsApp {
+    public void enviar(String reporte){
+        System.out.println("Enviar reporte por WhatsApp:"+reporte);
+    }
+    
+}
